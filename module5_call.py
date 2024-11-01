07ab6d0e9b3c1ae8c883c49bda6be089eaf3d5e9
onkar-sawant-1 <onkarpradeep.sawant@sofia.edu>
2024-11-01 15:10:25 -0700

    Add files via upload

---
+++ module5_call.py
@@ -0,0 +1,11 @@
+import module5_mod
+
+
+def main():
+    numbers = module5_mod.NumberSearch()
+    numbers.read_numbers()
+    numbers.search_number()
+
+
+if __name__ == '__main__':
+    main()
---
+++ module5_oop.py
@@ -0,0 +1,28 @@
+class NumberSearch:
+    def __init__(self):
+        self.numbers = {}
+        self.n = 1
+
+    def read_numbers(self):
+        count = self.readN("How many numbers would you like to search? ")
+        for i in range(count):
+            number = self.readN(f"Enter number {i + 1}: ")
+            self.numbers[number] = self.n
+            self.n += 1
+
+    def search_number(self):
+        x = self.readN("Enter number to search: ")
+        print(self.numbers[x] if x in self.numbers else -1)
+
+    def readN(self, prompt):
+        while True:
+            try:
+                return int(input(prompt))
+            except ValueError:
+                print("Invalid input. Please enter an integer.")
+
+
+if __name__ == '__main__':
+    numbers = NumberSearch()
+    numbers.read_numbers()
+    numbers.search_number()
